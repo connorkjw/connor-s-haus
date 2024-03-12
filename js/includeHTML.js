692f737ed68445b0bd72adc89a983f2b0dd015da
고진우 노트북 <design@git.kculture.link>
2024-03-12 19:35:21 +0900

    init

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // 문서준비가 완료된 이후에 동작해야하는 스크립트 작성!!
+    includeHTML();
+    
+});
+
 function includeHTML() {
     var headerElement = document.querySelector('header[include-html]');
     var footerElement = document.querySelector('footer[include-html]');
@@ -7,6 +13,14 @@ function includeHTML() {
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 headerElement.innerHTML = this.responseText;
+
+                document.getElementsByClassName('menuShow')[0].addEventListener('click', function(){
+                    document.getElementsByClassName('full_screen_container')[0].style.display = 'block';
+                })
+                
+                document.getElementById('menuHide').addEventListener('click', function(){
+                    document.getElementsByClassName('full_screen_container')[0].style.display = 'none';
+                }) 
             }
         };
         xhttp.open("GET", headerElement.getAttribute('include-html'), true);
@@ -24,3 +38,4 @@ function includeHTML() {
         xhttp2.send();
     }
 }
+
